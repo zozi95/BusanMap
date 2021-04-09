c3093c07d82c596b9f1668a288b29b0ed907c86a
kyun <zozi95@naver.com>
2021-04-09 13:44:33 +0900

    04.09 13:44

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 createFlowers();
 
-setInterval(createFlowers, 140);
+setInterval(createFlowers, 200);
 
 function createFlowers() {
   const flowers = document.createElement("i");
   flowers.classList.add("fas");
   flowers.classList.add("fa-egg");
   flowers.style.left = Math.random() * window.innerWidth + "px";
-  flowers.style.animationDuration = Math.random() * 10 + 5 + "s";
+  flowers.style.animationDuration = Math.random() * 20 + 10 + "s";
   flowers.style.opacity = Math.random();
   flowers.style.fontSize = Math.random() * 2 + 5 + "px";
 
